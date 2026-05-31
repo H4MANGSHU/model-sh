6cd5d022c16d6448010a48c35ddb8ecabebe068b
H4MANGSHU <himangshugoswami112@gmail.com>
2026-05-31 07:05:04 -0700

    Update install_comfyui.sh

--- install_comfyui.sh
+++ install_comfyui.sh
@@ -12,15 +12,3 @@ if [ ! -d "/content/ComfyUI" ]; then
   git clone https://github.com/comfyanonymous/ComfyUI.git /content/ComfyUI
 fi
 
-cd /content/ComfyUI
-
-echo "Installing Python requirements..."
-pip install -r requirements.txt
-
-echo "Installing ComfyUI Manager..."
-cd /content/ComfyUI/custom_nodes
-if [ ! -d "ComfyUI-Manager" ]; then
-  git clone https://github.com/ltdrdata/ComfyUI-Manager.git
-fi
-
-echo "ComfyUI installed."
