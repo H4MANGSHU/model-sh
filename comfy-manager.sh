22d34045e40d4b1f3dc43492c7486232e204169e
H4MANGSHU <himangshugoswami112@gmail.com>
2026-05-31 07:25:48 -0700

    Update comfy-manager.sh

--- comfy-manager.sh
+++ comfy-manager.sh
@@ -1,9 +1,18 @@
-cd /content/ComfyUI
+%cd /content/model-sh
+!sed -i 's/\r$//' *.sh
+!chmod +x *.sh
 
-echo "Installing Python requirements..."
-pip install -r requirements.txt
+%cd /content/ComfyUI
+!pip install -U --pre comfyui-manager
 
-echo "Installing ComfyUI Manager..."
-cd /content/ComfyUI/custom_nodes
-if [ ! -d "ComfyUI-Manager" ]; then
-  git clone https://github.com/ltdrdata/ComfyUI-Manager.git
+%cd /content/ComfyUI/custom_nodes
+!rm -rf ComfyUI-Manager
+!git clone https://github.com/ltdrdata/ComfyUI-Manager.git
+
+%cd /content/ComfyUI/custom_nodes/ComfyUI-Manager
+!pip install -r requirements.txt || true
+
+%cd /content/model-sh
+!sed -i 's/python main.py --listen 0.0.0.0 --port 8188/python main.py --listen 0.0.0.0 --port 8188 --enable-manager/g' start_comfyui_tunnel.sh
+
+!bash start_comfyui_tunnel.sh
